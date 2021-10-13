1720a1793c0b2c49cb4235a935ce3c77eb2bc348
Omkar Jawaji <omkarjawaji9@gmail.com>
2021-10-14 03:10:29 +0530

    First Commit with basic webapp laid out having a functionality of basic URL routing

---
+++ projects/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+projectsList = [
+    {
+        'id':'1',
+        'title':"Ecommerce Website",
+        'description':'Fully functional ecommerce website'
+    },
+        {
+        'id':'2',
+        'title':"Portfolio Website",
+        'description':'This was a project where I built out my portfolio'
+    },
+        {
+        'id':'3',
+        'title':"Social Network",
+        'description':'Awesome open source project I am still working'
+    },
+]
+
+def projects(request):
+    page = 'projects'
+    number = 10
+    context = {'page': page, 'number': number, 'projects': projectsList}
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project':projectObj})
+
