f7a1062babf105fe6541d2cf916912d35458e47f
Omkar Jawaji <omkarjawaji9@gmail.com>
2021-10-24 05:19:54 +0530

    CRUD operations are working now

--- projects/models.py
+++ projects/models.py
@@ -2,12 +2,16 @@ from enum import unique
 from django.db import models
 import uuid
 
+#models here
+
+
+
 class Project(models.Model):
     title = models.CharField(max_length = 200)
     description = models.TextField(null = True, blank = True)
     demo_link = models.CharField(max_length = 2000, null = True, blank = True)
     source_link = models.CharField(max_length = 2000, null = True, blank = True)
-    tags = models.ManyToManyField('Tag', blank = True)#relationship name
+    tags = models.ManyToManyField('Tag', blank = True) # relationship name
     vote_total = models.IntegerField(default = 0, null = True, blank= True)
     vote_ratio = models.IntegerField(default = 0, null = True, blank= True)
     created = models.DateTimeField(auto_now_add = True)
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
+from .forms import ProjectForm
 
+'''
 projectsList = [
     {
         'id':'1',
@@ -19,7 +21,7 @@ projectsList = [
         'description':'Awesome open source project I am still working'
     },
 ]
-
+'''
 
 
 def projects(request):
@@ -31,3 +33,37 @@ def project(request, pk):
     projectObj = Project.objects.get(id = pk)
     return render(request, 'projects/single-project.html', {'project' : projectObj})
 
+def createProject(request):
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        #print(request.POST)
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form':form} #context dictionary
+    return render(request, "projects/project_form.html", context)
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance = project)
+
+    if request.method == 'POST':
+        #print(request.POST)
+        form = ProjectForm(request.POST, instance = project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form':form} #context dictionary
+    return render(request, "projects/project_form.html", context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+
+    if request.method == "POST":
+        project.delete()
+        return redirect('projects')
+
+    context = {'object':project}
+    return render(request, 'projects/delete_template.html', context)
